fe08a19ba692b3ea7c53051352fdeec2a2c7a3d6
jack <chengjunjie.jack@gmail.com>
2024-04-29 23:44:39 +0800

    修复错误

--- vm/vm_test.c
+++ vm/vm_test.c
@@ -10,12 +10,12 @@ int main(void)
   int* a = mm_malloc(sizeof(int));
   *a = 12;
 
-  printf("%x: %d\n", a, *a);
+  printf("%p: %d\n", a, *a);
   mm_free(a);
 
   int* b = mm_malloc(sizeof(int));
   *b = 101;
-  printf("%x: %d\n", b, *b);
+  printf("%p: %d\n", b, *b);
   mm_free(b);
   return 0;
 }
