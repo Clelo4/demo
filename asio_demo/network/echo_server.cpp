f0c8b126dce7b7345f4b10b9321b8d0f380b0ca3
jack <chengjunjie.jack@gmail.com>
2024-06-19 00:38:33 +0800

    fix错误

--- asio_demo/network/echo_server.cpp
+++ asio_demo/network/echo_server.cpp
@@ -4,11 +4,11 @@
 #include <boost/system/detail/error_code.hpp>
 #include <iostream>
 
-void handle_client(boost::asio::ip::tcp::socket &socket) {
+void handle_client(std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr) {
   boost::asio::streambuf buffer;
   boost::system::error_code error;
 
-  boost::asio::read_until(socket, buffer, '\n', error);
+  boost::asio::read_until(*socket_ptr, buffer, '\n', error);
   if (error) {
     std::cerr << "Error reading data: " << error.message() << std::endl;
     return;
@@ -16,7 +16,7 @@ void handle_client(boost::asio::ip::tcp::socket &socket) {
 
   // Echo the data back to the client
   std::string message = boost::asio::buffer_cast<const char *>(buffer.data());
-  boost::asio::write(socket, boost::asio::buffer(message), error);
+  boost::asio::write(*socket_ptr, boost::asio::buffer(message), error);
   if (error) {
     std::cerr << "Error writing data: " << error.message() << std::endl;
   }
@@ -31,14 +31,14 @@ int main() {
   std::cout << "TCP Echo Server started. Listening on port 8888." << std::endl;
 
   while (true) {
-    boost::asio::ip::tcp::socket socket(io_context);
-    acceptor.accept(socket);
+    std::shared_ptr<boost::asio::ip::tcp::socket> socket_ptr = std::make_shared<boost::asio::ip::tcp::socket>(io_context);
+    acceptor.accept(*socket_ptr);
 
-    std::cout << "New connection from: " << socket.remote_endpoint()
+    std::cout << "New connection from: " << socket_ptr->remote_endpoint()
               << std::endl;
 
     // Handle the client in a separate thread
-    std::thread(handle_client, std::ref(socket)).detach();
+    std::thread(handle_client, socket_ptr).detach();
   }
   return 0;
 }
